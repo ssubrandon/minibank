ca2a368600c3a0fd02997e6cb9f475d57db14fa0
SSUBrandon <brandon1380.bw@gmail.com>
2018-09-25 23:41:00 -0700

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS minibank DEFAULT CHARACTER SET utf8mb4;
+
+USE minibank;
+
+CREATE TABLE IF NOT EXISTS account ( 
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    username CHAR(30) NOT NULL,
+    password CHAR(60) NOT NULL,
+    timestamp BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE KEY(username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE USER IF NOT EXISTS 'minibank'@'%' IDENTIFIED by 'minibank';
+
+GRANT ALL PRIVILEGES ON minibank.* to 'minibank'@'%';
+
+FLUSH PRIVILEGES;
